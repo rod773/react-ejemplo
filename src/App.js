f1a8441c2a16f675509937f23ffe4c8b60ca3e50
rod773 <104315743+rod773@users.noreply.github.com>
2023-09-30 21:38:31 +0200

    button get data

--- src/App.js
+++ src/App.js
@@ -3,26 +3,24 @@ import "./App.css";
 import axios from "axios";
 
 function App() {
-  const res = axios.get("https://randomuser.me/api/");
+  const getData = () => {
+    axios
+      .get("https://randomuser.me/api/")
 
-  console.log(res);
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>React Application - This is My App</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={getData}>Get Data</button>
       </header>
     </div>
   );
